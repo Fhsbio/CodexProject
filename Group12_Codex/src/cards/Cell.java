838580b8e8bd8920b27f9ccd7a38a1a03df3b1b9
Mariie49 <128154830+Mariie49@users.noreply.github.com>
2024-05-07 22:59:47 +0200

    Update Cell.java

--- Group12_Codex/src/cards/Cell.java
+++ Group12_Codex/src/cards/Cell.java
@@ -1,5 +1,5 @@
 package cards;
-
+//non penso servi questa classe
 public class Cell {
 	private Object obj;
 	private Resources resource;
@@ -27,42 +27,4 @@ public class Cell {
 		return coordinata;
 		}
 
-
-	/*
-
-	 * public Cell(Coordinata coordinata) {
-	 * 
-	// chiama il costruttore di coordinata
-	setPezzo(null);
-	this.coordinata = coordinata;
-	}
-	public SymbolOfCell getSymbol() {
-	return ;
-	}
-	public void setPezzo(Pezzo pezzo) {
-	this.pezzo = pezzo;
-	}
-	public boolean isEmpty() {
-	if (this.pezzo == null) {
-	return true;
-	}
-	return false;
-	}
-	public Pezzo unsetPezzo() {
-	Pezzo pieceToBeReturned = this.pezzo;
-	this.pezzo = null;
-	return pieceToBeReturned;
-	}
-	@Override
-	public String toString() {
-	if (this.getPezzo() != null) {
-	return this.getPezzo().toString();
-	} else {
-	return " ";
-	}
-	}
-	
-
-
-	 */
 }
